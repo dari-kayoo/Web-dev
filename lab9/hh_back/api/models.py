72ba60150b30da0c54c4ef5236df51474240c636
dari-kayoo <d_zhakysh@kbtu.kz>
2022-04-15 19:07:37 +0600

    commit

---
+++ lab9/hh_back/api/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    address = models.TextField()
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+class Vacancy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    salary = models.FloatField(default=0)
+    company = models.ForeignKey()
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company
+        }
---
+++ lab9/hh_back/api/views.py
@@ -0,0 +1,38 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from api.models import Company, Vacancy
+import operator
+
+# Create your views here.
+#a
+def companies_list(request):
+    companies = Company.objects.all()
+    companies_json = [companies.to_json() for company in companies]
+    return JsonResponse(companies_json, safe=0)
+#b
+def companies_detail(request, company_id):
+    try:
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'message' : str(e)})
+    return JsonResponse(company.to_json())
+
+#d
+def vacancies_list(request):
+    vacancies = Vacancy.objects.all()
+    vacancies_json = [vacancies.to_json() for vacancy in vacancies]
+    return JsonResponse(vacancies_json, safe=0)
+
+#e
+def vacancies_detail(request, vacancy_id):
+    try:
+        vacancy = Vacancy.objects.get(id=vacancy_id)
+    except Vacancy.DoesNotExist as e:
+        return JsonResponse({'message':str(e)})
+    return JsonResponse(vacancy.to_json())
+
+#f
+def vacancy_top_ten():
+    auths = Vacancy.objects.order_by('-salary')[:10]
+    ordered = sorted(auths, key=operator.attrgetter('name'))
+    return JsonResponse(ordered.to_json())
